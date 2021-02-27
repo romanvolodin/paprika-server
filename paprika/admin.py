0519cd3185e3c3894c2f7ef669a2881e596bbef2
Roman Volodin <roman.volodin@gmail.com>
2021-02-28 01:56:23 +0300

    Add Project model

--- paprika/admin.py
+++ paprika/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import User
+from .models import (
+    Project,
+    User,
+)
 
-
+admin.site.register(Project)
 admin.site.register(User)
--- paprika/models.py
+++ paprika/models.py
@@ -17,3 +17,21 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Project(models.Model):
+    title = models.CharField('Название проекта', max_length=255)
+    code = models.CharField('Код проекта', max_length=5)
+    preview = models.ImageField('Превью', blank=True, null=True)
+    deadline = models.DateTimeField('Дедлайн', blank=True, null=True)
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Автор',
+        related_name='projects'
+    )
+
+    def __str__(self):
+        return self.code
