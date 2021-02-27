7958d53d960cf6c03ef9d72d01d571f647acb2ee
Roman Volodin <roman.volodin@gmail.com>
2021-02-28 02:32:40 +0300

    Setup Django to serve uploaded files in Debug mode

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -5,3 +7,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
