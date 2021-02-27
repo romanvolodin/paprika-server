f3fcc168bea37bed5e8051755d5d51d84f40d291
Roman Volodin <roman.volodin@gmail.com>
2021-02-28 02:34:04 +0300

    Remove unused import

--- paprika/models.py
+++ paprika/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 from .managers import UserManager
 
