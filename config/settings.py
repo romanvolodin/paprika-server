8f18fc4c260a65ee83d635d7075d2b7de13750f7
Roman Volodin <roman.volodin@gmail.com>
2021-03-02 01:21:24 +0300

    Fix STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
